f4cd58496c55b960308329a6081b9f316d82028c
Ahan <jiangjiafu1989@gmail.com>
2015-05-28 09:52:22 -0400

    1.Put the source files to right directories.

---
+++ header_demo/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+//#include <b.h>
+#include "a.h"
+#include "b.h"
+
+int main()
+{
+    printf("ok\n");
+    printf("sizeof TYPE_B_T:%d\n", sizeof(TYPE_B_T));
+    return 0;
+}
